29e085ee07f4a586af01e9a897135341ea3931ba
yline <ryuyoseon@rysMAC.local>
2021-10-21 16:13:47 +0900

    test code
    - test jpa
    - test h2 database

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    // 커맨드랑 쿼리를 분리해라 (사이트 이펙트 일으키는 커멘드성)
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}
