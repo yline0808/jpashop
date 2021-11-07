40c1f5753ebddce5b16e8a09288d02b0d7dc4bb0
yline <ryuyoseon@rysMAC.local>
2021-11-07 19:45:25 +0900

    create controller
    - add member join, search
    - add item save, update, search

--- src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -26,6 +26,6 @@ public class ItemRepository {
     }
 
     public List<Item> findAll() {
-        return em.createQuery("select i form Item i", Item.class).getResultList();
+        return em.createQuery("select i from Item i", Item.class).getResultList();
     }
 }
